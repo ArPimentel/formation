82a31fe206911d3429cedc44220f3678cf17f62c
Arturo PIMENTEL <48420322+ArPimentel@users.noreply.github.com>
2022-12-11 21:21:32 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+const message =
+  "Thank you for submitting your request. The crew will respond shortly.";
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert(message);
+  });
